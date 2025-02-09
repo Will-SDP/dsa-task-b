39009d3cd83111ac694a08c8598443c54866bd10
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-02-09 15:30:36 +0000

    Seperated plane class

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
 import heapq 
-
-class Plane:
-    def __init__(self, label, fuel, distance):
-        self.__label = label
-        self.__fuel = fuel
-        self.__distance = distance
-        self.__priority = None
-        self.calculate_priority(self.__fuel, self.__distance)
-
-    def __lt__(self, obj):
-        return self.__priority < obj.__priority    
-    
-    def __repr__(self):
-        return f"Plane({self.__label}, Priority={self.__priority})"    
-
-    def calculate_priority(self, fuel, distance):
-        if self.__priority != 0:
-            self.__priority = abs(fuel - distance) + (fuel + distance)
-    
-    def get_priority(self):
-        return int(self.__priority)
-    
-    def get_label(self):
-        return self.__label
-    
-    def mayday(self):
-        self.__priority = 0
-
+from plane import Plane
 
 planes = [    
     {'id': 'PlaneA', 'distance': 50, 'fuel': 20},
